93bec9f56650ec468936568632edaa25d5cc965f
Alexey S <huji@mail.ru>
2025-09-21 21:28:21 +0500

    fix

--- otipb_bot.py
+++ otipb_bot.py
@@ -1,19 +1,72 @@
 import asyncio
 import logging
 
-# from aiogram.filters.command import Command
-# from aiogram.fsm.context import FSMContext
-# from aiogram.types import MessageReactionUpdated
+from aiogram import F
+
+from aiogram.filters.command import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from config.bot_config import bot, dp
+from config.telegram_config import MY_TELEGRAM_ID, GROUP_ID
 from handlers import service
+from utils.utils import report_error
 
+
+MSG_TEXT = ('Прошу предоставить фотографии журналов и актов АПК в данную группу до 12:00. '
+            'Для автоматического учёта отправленных сообщений прошу указывать службу (в виде контекста или отдельным сообщением).')
+
+
+# @dp.message(F.content_type.in_({'text', 'video', 'photo', 'document'}))
+# async def archive_messages(message: Message):
+#     chat = message.chat.id
+#     thread = message.message_thread_id
+#     if message.text:
+#         await bot.send_message(
+#             chat_id=MY_TELEGRAM_ID,
+#             text=f'{message.chat.id} {thread} принято',
+#             parse_mode='HTML'
+#         )
+
+
+async def send_reminder():
+    try:
+        await bot.send_message(chat_id=GROUP_ID, text=MSG_TEXT)
+    except Exception as e:
+        await report_error(e)
+
+
+# удаление сервисных сообщений
+@dp.message(
+        F.content_type.in_([
+            'pinned_message',
+            'left_chat_member',
+            'forum_topic_created',
+            'forum_topic_closed',
+            'forum_topic_edited',
+            'forum_topic_reopened',
+            'new_chat_members'
+        ])
+    )
+async def delete_service_pinned_message(message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
 
 
 async def main():
-    dp.include_routers(
-        service.router,
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(
+        send_reminder,
+        'cron',
+        day_of_week='mon',
+        hour=8,
+        minute=30,
+        timezone='Asia/Yekaterinburg'
     )
+    dp.include_routers(service.router)
     await dp.start_polling(bot)
 
 
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,9 @@
 import random
+import inspect
+import logging
+
+from config.bot_config import bot
+from config.telegram_config import MY_TELEGRAM_ID
 
 
 def random_list_elem(list):
@@ -17,3 +22,24 @@ def word_conjugate(number):
     if 1 < last_digit < 5 and last_two_digit not in range(11, 15):
         return f'{args[1]}'  # заявки
     return f'{args[2]}'  # заявок
+
+
+async def report_error(e: Exception):
+    """Уведомляет администратора и логирует ошибку"""
+    # Получаем стек вызовов
+    frame = inspect.stack()[1]
+    func_name = frame.function
+    file_name = frame.filename
+    line_number = frame.lineno
+
+    # Текст ошибки
+    error_text = f"Ошибка в {func_name} ({file_name}, строка {line_number}): {str(e)}"
+
+    # Логируем ошибку в файл
+    logging.error(error_text)
+
+    # Пытаемся отправить сообщение админу
+    try:
+        await bot.send_message(MY_TELEGRAM_ID, text=f'❗️{error_text}')
+    except Exception as inner_e:
+        logging.error(f"Ошибка при отправке уведомления администратору: {str(inner_e)}")
